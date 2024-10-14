1a437adcd28a772deafd54e5dca2c22edc7eab3c
JorgeFVS <jorgevieira99@hotmail.com>
2024-10-14 18:47:00 -0300

    InDeleção de usuario com delete

--- src/main/java/com/jorge/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jorge/workshopmongo/resources/UserResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -39,5 +38,11 @@ public class UserResource {
         user = userService.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/jorge/workshopmongo/services/UserService.java
+++ src/main/java/com/jorge/workshopmongo/services/UserService.java
@@ -29,6 +29,12 @@ public class UserService {
         return userRepository.insert(newUser);
     }
 
+    public void delete(String id) {
+        findById(id);
+
+        userRepository.deleteById(id);
+    }
+
     public User fromDTO(UserDTO userDTO) {
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
