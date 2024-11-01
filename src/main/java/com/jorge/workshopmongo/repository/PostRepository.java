c1a7e2b71e29d37f68d2796935e379191318dfae
JorgeFVS <jorgevieira99@hotmail.com>
2024-11-01 17:45:45 -0300

    Consulta simples com query methods

--- src/main/java/com/jorge/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jorge/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,12 @@ package com.jorge.workshopmongo.repository;
 
 import com.jorge.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/jorge/workshopmongo/services/PostService.java
+++ src/main/java/com/jorge/workshopmongo/services/PostService.java
@@ -5,6 +5,7 @@ import com.jorge.workshopmongo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,5 +17,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> posts = postRepository.findById(id);
         return posts.orElseThrow(() -> new RuntimeException("Post not found"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
